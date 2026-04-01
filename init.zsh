edd03a31af0b40bedae798a2cddf11240ec8c5aa
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 11:04:24 -0400

    refactor(pusher): replace prompt::mod with profile::mod using p6_return_words, add README

--- init.zsh
+++ init.zsh
@@ -7,70 +7,21 @@
 #>
 ######################################################################
 p6df::modules::pusher::deps() {
-  ModuleDeps=()
+  ModuleDeps=(p6m7g8-dotfiles/p6common)
 }
 
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::pusher::prompt::mod()
+# Function: words pusher $PUSHER_APP_ID = p6df::modules::pusher::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - pusher $PUSHER_APP_ID
 #
-#  Environment:	 P6_DFZ_PROFILE_PUSHER
+#  Environment:	 PUSHER_APP_ID
 #>
 ######################################################################
-p6df::modules::pusher::prompt::mod() {
-  local str=""
-  local profile="$P6_DFZ_PROFILE_PUSHER"
+p6df::modules::pusher::profile::mod() {
 
-  if p6_string_blank_NOT "$profile"; then
-    str="pusher:\t\t  ${profile}"
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::pusher::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block
-#
-#  Environment:	 P6_DFZ_PROFILE_PUSHER
-#>
-######################################################################
-p6df::modules::pusher::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_PUSHER" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::pusher::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 P6_DFZ_PROFILE_PUSHER
-#>
-######################################################################
-p6df::modules::pusher::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_PUSHER
-
-  p6_return_void
+  p6_return_words 'pusher' '$PUSHER_APP_ID'
 }
